f8fe4c46e6e3cfd57376334d3d67b00010a77a52
Emaad Khan <e4emaad@gmail.com>
2026-03-09 12:26:46 -0400

    Initialize carsror application in main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// carsror - ReXGlue Recompiled Project
+//
+// This file is yours to edit. 'rexglue migrate' will NOT overwrite it.
+
+#include "generated/carsror_config.h"
+#include "generated/carsror_init.h"
+
+#include "carsror_app.h"
+
+REX_DEFINE_APP(carsror, CarsrorApp::Create)
